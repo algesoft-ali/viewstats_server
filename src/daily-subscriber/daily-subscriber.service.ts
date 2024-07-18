f44b710b79201665439d59715d4dac2cbe5c0549
algesoft-ali <mohammad.ali@algesoft.com>
2024-07-18 16:56:26 +0600

    rate added to daily subscribers

--- src/daily-subscriber/daily-subscriber.service.ts
+++ src/daily-subscriber/daily-subscriber.service.ts
@@ -58,13 +58,33 @@ export class DailySubscriberService {
           updatedAt: 0,
         },
         {
-          sort: { date: -1 },
+          sort: { date: 1 },
         }
       )
       .exec();
 
+    // Calculate increment/decrement rate
+    const dataWithRate = data.map((current, index, array) => {
+      if (index === 0) {
+        return { ...current.toObject(), rate: 0 }; // No previous data to compare for the first item
+      }
+
+      const previous = array[index - 1];
+      let rate: number = 0;
+
+      if (previous.subscribers !== 0) {
+        rate = +(
+          ((current.subscribers - previous.subscribers) /
+            previous.subscribers) *
+          100
+        ).toFixed(2);
+      }
+
+      return { ...current.toObject(), rate };
+    });
+
     const total = await this.dailySubscriberModel.countDocuments(filter);
 
-    return { data, total };
+    return { data: dataWithRate, total };
   }
 }

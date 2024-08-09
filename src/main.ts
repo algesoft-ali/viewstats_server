dda1fdc05b93773f30dd0e69d21718adeea1e7e5
algesoft-ali <mohammad.ali@algesoft.com>
2024-08-09 11:58:07 +0600

    Swagger Setup Updated

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,10 @@ async function bootstrap() {
   // ----- Swagger
   const swaggerConfig = new DocumentBuilder()
     .setTitle("ViewStats API")
-    .setDescription("ViewStats Server API")
+    .setDescription(
+      "This is dummy & testing project of integrating Nest JS backend API's. All of the API's are integrated with RTK Query and the state managed with Redux Toolkit. (Right Now Only 'MrBeast' Channel has daily views & daily subscribers data. Others channel doesn't have that data yet!)"
+    )
+    .setExternalDoc("Visit Here", "https://viewstats-xi.vercel.app/")
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
